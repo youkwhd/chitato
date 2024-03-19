49a61a818aa719a127ccd4234b192bf61b429eb0
youkwhd <lolywk@tutanota.com>
2024-03-19 14:49:37 +0700

    chore: remove unused params, include player characters

--- src/board.lisp
+++ src/board.lisp
@@ -3,26 +3,31 @@
 (require :cl-charms)
 
 (defconstant +board+
-  '((0 0 0)
-    (0 0 0)
-    (0 0 0)))
+  '((:e :e :e)
+    (:e :e :e)
+    (:e :e :e))
+  "board :e for empty, :x, :o are players")
 
 (defun hello (to)
   (format t "Hello, ~a!~%" to))
 
-(defun draw-board-line (curses-window board w h)
+(defun draw-board-line (curses-window board)
   (loop for _ from 1 to (length board) do
     (charms:write-string-at-cursor curses-window "+")
     (charms:write-string-at-cursor curses-window "---"))
   (charms:write-string-at-cursor curses-window "+")
   (charms:write-char-at-cursor curses-window #\newline))
 
-(defun draw-board-middle (curses-window board-row w h)
+(defun draw-board-middle (curses-window board-row)
   (loop for x in board-row do
     (charms:write-string-at-cursor curses-window "| ")
     (case x
-      ((0)
-       (charms:write-char-at-cursor curses-window #\space)))
+      ((:e)
+       (charms:write-char-at-cursor curses-window #\space))
+      ((:x)
+       (charms:write-char-at-cursor curses-window #\X))
+      ((:o)
+       (charms:write-char-at-cursor curses-window #\O)))
     (charms:write-char-at-cursor curses-window #\space))
   (charms:write-string-at-cursor curses-window "|")
   (charms:write-char-at-cursor curses-window #\newline))
@@ -32,7 +37,7 @@
   (charms:refresh-window curses-window)
 
   (dolist (row board)
-      (draw-board-line curses-window board 0 0)
-      (draw-board-middle curses-window row 0 0))
+      (draw-board-line curses-window board)
+      (draw-board-middle curses-window row))
 
-  (draw-board-line curses-window board 0 0))
+  (draw-board-line curses-window board))

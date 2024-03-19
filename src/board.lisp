133506e6b8ea8df904136e4bc47cdacc866b8027
youkwhd <lolywk@tutanota.com>
2024-03-19 12:56:03 +0700

    fix: call quote just once

--- src/board.lisp
+++ src/board.lisp
@@ -1,9 +1,9 @@
 (in-package :chitato)
 
 (defconstant +board+
-  '('(0 0 0)
-    '(0 0 0)
-    '(0 0 0)))
+  '((0 0 0)
+    (0 0 0)
+    (0 0 0)))
 
 (defun hello (to)
   (format t "Hello, ~a!~%" to))

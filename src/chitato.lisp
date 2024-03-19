e25f95ac4c0c0f26b4fb3956cb5502f36454a09a
youkwhd <lolywk@tutanota.com>
2024-03-19 10:34:20 +0700

    feat: initialize charms, quit on q

--- src/chitato.lisp
+++ src/chitato.lisp
@@ -8,4 +8,19 @@
 (load "src/board.lisp")
 
 (defun main ()
-  (hello "Common Lisp"))
+  (let ((stdwin (charms:initialize)))
+    (charms:disable-echoing)
+    (charms:enable-raw-input :interpret-control-characters t)
+    (charms:enable-non-blocking-mode stdwin)
+
+    (loop
+      (charms:clear-window stdwin)
+      (charms:refresh-window stdwin)
+
+      (let ((ch (charms:get-char stdwin
+                       :ignore-error t)))
+        (case ch
+          ((nil) nil)
+          ((#\q)
+           (charms:finalize)
+           (sb-ext:exit)))))))
